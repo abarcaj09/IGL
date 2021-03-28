c7543ad54134c183c5b86ccfc8ea5ef34e954048
Juan Abarca <abarcaj277@gmail.com>
2021-03-28 16:52:11 -0700

    created user reducer & added setUser action

---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,68 @@
+import usersService from "../services/users";
+
+const initialState = {
+  // the current user's info
+  id: "",
+  username: "",
+  name: "",
+  profilePic: "",
+  biography: "",
+  followers: [],
+  following: [],
+
+  homePosts: [], // when url = /
+  homePostsLoaded: false,
+  homePostsError: "",
+
+  suggestions: [], // when url = /
+  suggestionsLoaded: false,
+  suggestionsError: "",
+
+  explorePosts: [], // when url = /explore
+  explorePostsLoaded: false,
+  explorePostsError: "",
+
+  profileToView: null, // when url = /:username
+  success: false,
+  error: "",
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_USER": // sets the current user's info
+      const userFollowing = action.payload.following.map((user) => user.id);
+      return {
+        ...state,
+        ...action.payload,
+        following: userFollowing,
+        // likes, saved, & posts will be part of posts reducer
+        likes: null,
+        saved: null,
+        posts: null,
+      };
+    case "SET_USER_ERROR":
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const setUser = (username, config) => {
+  return async (dispatch) => {
+    const userAccount = await usersService.getUser(username, config);
+
+    if (userAccount.error) {
+      return dispatch({
+        type: "SET_USER_ERROR",
+        payload: userAccount.error,
+      });
+    }
+
+    dispatch({
+      type: "SET_USER",
+      payload: userAccount,
+    });
+  };
+};
+
+export default userReducer;
