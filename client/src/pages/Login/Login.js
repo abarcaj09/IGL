012bcb78b07e7e634afaffae7723bd951d311326
Juan Abarca <abarcaj277@gmail.com>
2021-03-27 22:03:56 -0700

    completed Login page and its styles

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,10 +1,80 @@
 import "./Login.css";
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { login } from "../../reducers/authReducer";
+import { useDispatch, useSelector } from "react-redux";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const error = useSelector(({ auth }) => {
+    return auth.error;
+  });
+
+  const [account, setAccount] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login({ account, password }));
+  };
+
   return (
     <div className="login">
-      <h1>Login Page</h1>
+      <div className="login-container">
+        <div className="login-top">
+          <div className="login-logoContainer">
+            <img className="login-logo" src="/igl-logo.png" alt="logo" />
+          </div>
+
+          <div className="login-form">
+            <form onSubmit={onSubmit}>
+              <div className="login-input">
+                <label>Username or email</label>
+                <input
+                  type="text"
+                  name="account"
+                  value={account}
+                  onChange={(e) => setAccount(e.target.value)}
+                  autoFocus
+                />
+              </div>
+
+              <div className="login-input">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <button
+                className="login-button"
+                disabled={!account || password.length < 6}
+                type="submit"
+              >
+                Log In
+              </button>
+
+              {error && (
+                <div className="login-error">
+                  <h3>{error}</h3>
+                </div>
+              )}
+            </form>
+          </div>
+        </div>
+
+        <div className="login-bottom">
+          <p>
+            Don't have an account?
+            <Link to="/accounts/register" className="login-registerLink">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
