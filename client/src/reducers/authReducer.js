9f536ccf64f7fcba78f43f8db7d9b88b7dfada50
Juan Abarca <abarcaj277@gmail.com>
2021-03-27 20:58:20 -0700

    login and logout actions added to auth reducer. Also added retrieval of jwt from local storage

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,11 +1,28 @@
 import authService from "../services/auth";
 import { getConfig } from "../utils";
+import jwt_decode from "jwt-decode";
 
 const initialState = {
   user: null, // object contains username and jwt
   error: null,
   config: null, // contains Authorization header with jwt
 };
+
+const storedJWT = localStorage.getItem("userJWT");
+if (storedJWT) {
+  const decodedToken = jwt_decode(storedJWT);
+
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("userJWT");
+  } else {
+    // set user and authHeader if valid jwt is stored
+    initialState.user = {
+      token: storedJWT,
+      username: decodedToken.username,
+    };
+    initialState.config = getConfig(storedJWT);
+  }
+}
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -17,7 +34,9 @@ const authReducer = (state = initialState, action) => {
         error: null,
         config: getConfig(action.payload.token),
       };
-
+    case "LOGOUT":
+      localStorage.removeItem("userJWT");
+      return action.payload;
     case "ERROR":
       return { ...state, error: action.payload };
     default:
@@ -40,6 +59,31 @@ export const register = (userInfo) => {
       type: "LOGIN",
       payload: registeredUser,
     });
+  };
+};
+
+export const login = (userCredentials) => {
+  return async (dispatch) => {
+    const authUser = await authService.login(userCredentials);
+
+    if (authUser.error) {
+      return dispatch({
+        type: "ERROR",
+        payload: authUser.error,
+      });
+    }
+
+    dispatch({
+      type: "LOGIN",
+      payload: authUser,
+    });
+  };
+};
+
+export const logout = () => {
+  return {
+    type: "LOGOUT",
+    payload: { user: null, error: null, config: null },
   };
 };
 
