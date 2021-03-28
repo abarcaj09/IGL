860688b6532a70e6dcd578568f35b79860716399
Juan Abarca <abarcaj277@gmail.com>
2021-03-28 16:59:32 -0700

    set the logged in user with a useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   Create,
   EditProfile,
@@ -13,8 +15,20 @@ import {
 import AuthRoute from "./components/AuthRoute";
 import UserRoute from "./components/UserRoute";
 import Header from "./components/Header";
+import { setUser } from "./reducers/userReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { user, config } = useSelector(({ auth }) => {
+    return auth;
+  });
+
+  useEffect(() => {
+    if (user) {
+      dispatch(setUser(user.username, config));
+    }
+  }, [dispatch, user, config]);
+
   return (
     <div className="app-body">
       <Router>
