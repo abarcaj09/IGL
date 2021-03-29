02e087e92f4ea3ce38689d608cbc66f642f13ec5
Juan Abarca <abarcaj277@gmail.com>
2021-03-28 17:04:22 -0700

    added user & posts (TODO) reducers to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import authReducer from "./reducers/authReducer";
+import userReducer from "./reducers/userReducer";
+// import postsReducer from "./reducers/postsReducer";
 
 const reducer = combineReducers({
   auth: authReducer,
+  user: userReducer,
+  // posts: postsReducer
 });
 
 const store = createStore(reducer, applyMiddleware(thunk));
