b144d171043fbd4f3fd4ea14fed119ddcc60b088
Juan Abarca <abarcaj277@gmail.com>
2021-03-28 18:40:59 -0700

    created debounce hook

---
+++ client/src/components/Header/useDebounce.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+// hook to prevent calls to api until the user has
+// stopped typing their search input
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
