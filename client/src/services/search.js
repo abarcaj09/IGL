9cdbd7bfde9784a76a74099cd58fe07cc7773cbe
Juan Abarca <abarcaj277@gmail.com>
2021-03-28 17:59:49 -0700

    added retrieval of search results to search service

---
+++ client/src/services/search.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const baseUrl = "https://igl-server.herokuapp.com/api/search";
+
+const searchUsers = async (searchInput) => {
+  try {
+    // search for users by name and username
+    const response = await axios.get(
+      `${baseUrl}/users?name=${searchInput}&username=${searchInput}`
+    );
+
+    return response.data.results;
+  } catch (error) {
+    return [];
+  }
+};
+
+export default { searchUsers };
