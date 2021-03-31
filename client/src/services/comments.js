cede245ab4ac72c1fc1ba424621c96c0a253087a
Juan Abarca <abarcaj277@gmail.com>
2021-03-30 22:33:19 -0700

    creation & deletion of comments added to comments service

---
+++ client/src/services/comments.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const baseUrl = "https://igl-server.herokuapp.com/api/comments";
+
+const newComment = async (commentObj, config) => {
+  try {
+    const response = await axios.post(baseUrl, commentObj, config);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+const deleteComment = async (id, config) => {
+  try {
+    const response = axios.delete(`${baseUrl}/${id}`, config);
+    return response.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+
+export default { newComment, deleteComment };
