c56820bc3e60deaebe38f672bb40b4cf653a54fd
Juan Abarca <abarcaj277@gmail.com>
2021-04-01 23:04:04 -0700

    completed Explore page & its styles

--- client/src/pages/Explore/Explore.js
+++ client/src/pages/Explore/Explore.js
@@ -1,10 +1,44 @@
 import "./Explore.css";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { initExplorePosts } from "../../reducers/userReducer";
+import PostPreviews from "../../components/PostPreviews";
 
 const Explore = () => {
+  const dispatch = useDispatch();
+  const {
+    explorePosts,
+    explorePostsLoaded,
+    explorePostsError,
+    username,
+  } = useSelector(({ user }) => {
+    return user;
+  });
+  const config = useSelector(({ auth }) => {
+    return auth.config;
+  });
+
+  useEffect(() => {
+    dispatch(initExplorePosts(username, config));
+  }, [dispatch, username, config]);
+
+  if (!explorePostsLoaded) {
+    return null;
+  }
+
   return (
     <div className="explore">
-      <h1>Explore Page</h1>
+      {explorePostsError ? (
+        <p>Failed loading posts.</p>
+      ) : (
+        <div>
+          {explorePosts.length > 0 ? (
+            <PostPreviews posts={explorePosts} />
+          ) : (
+            <p>No Explore posts at this time</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
